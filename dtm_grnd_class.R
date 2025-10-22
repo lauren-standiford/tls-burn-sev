5557b7998eb0bd0a24509c2cada01702c13f6981
lauren-standiford <lstandif@berkeley.edu>
2025-10-22 11:44:24 -0700

    code for ht norm

--- dtm_grnd_class.R
+++ dtm_grnd_class.R
@@ -1,6 +1,8 @@
 library(lidR)
 library(stringr)
 
+############ generate dtms ############
+
 tls <- "E:/"
 
 c <- c("c1", "c6")
@@ -44,4 +46,97 @@ for (file in initial_c_files) {
   
   i = i + 1
 
+}
+
+############ ht normalization ############
+
+tls <- "E:/"
+
+files <- list.files(
+  tls,
+  full.names = T,
+  recursive = T,
+  pattern = '\\.(tif|las)$'
+)
+
+c1_files <- str_subset(files, "\\bc1\\b|\\bc1_dtms\\b")
+
+file.info <- data.frame(
+  file = c1_files,
+  plot = str_extract(basename(c1_files), "\\d{4}"),
+  type = str_extract(c1_files, "\\.(tif|las)$")
+)
+
+dtm_files <- subset(file.info, type == ".tif")
+las_files <- subset(file.info, type == ".las")
+
+matched <- merge(dtm_files, las_files, by = "plot", suffixes = c(".tif", ".las"))
+
+i = 1
+
+for (i in seq_len(nrow(matched))) {
+  las_file <- matched$file.las[i]
+  dtm_file <- matched$file.tif[i]
+  
+  las <- readLAS(las_file)
+  dtm <- rast(dtm_file)
+  
+  las_norm <- las - dtm
+  
+  htnorm_file_name <- str_replace(las_norm, "\\.las$", "_htnorm.las")
+
+  writeLAS(las_norm, htnorm_file_name)
+  
+  i = i + 1
+}
+
+
+
+############ junk code while i was trying to figure out the ht norm stuff #############
+
+# c6_las <- str_subset(files, "\\bc6\\b")
+c1_tif <- str_subset(files, "\\bc1_dtms\\b")
+# c6_tif <- str_subset(files, "\\bc6_dtms\\b")
+
+c1_matched <- merge(c1_las, c1_tif, by = "", suffixes = c("tif", "las"))
+las_tif_files <- c(c1_las, c6_las, c1_tif, c6_tif)
+
+# file = initial_c_files[1]
+# las = readLAS(file, filter = '-keep_random_fraction 0.0001')
+
+###########
+
+dtm_files <- str_subset(files, "\\bc1_dtms\\b")
+las_files <- str_subset(files, "\\bc1\\b")
+
+file.info <- data.frame(
+  file = 
+)
+
+############
+
+i = 2
+
+for (file in las_tif_files) {
+  
+  message('Processing ', file)
+  message(i, ' of ', length(las_tif_files))
+  tictoc::tic()
+  las <- readLAS(file)
+  
+  dtm <- terra::rast("")
+  tictoc::toc()
+  
+  # classified_file_name <- str_replace(file, "\\.las$", "_grndcls.las")
+  # 
+  # writeLAS(classified_las, classified_file_name)
+  
+  dtm <- rasterize_terrain(classified_las, res = 0.25, algorithm = tin())
+  
+  htnorm_file_name <- str_replace(file, "\\.las$", "_htnorm.las")
+  
+  terra::writeRaster(dtm, dtm_file_name, overwrite = TRUE)
+  
+  i = i + 1
+  
 }
